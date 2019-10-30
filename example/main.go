2fd9f6b9a8e8eeff7666504dc0492a723fdb249a
Lasse Martin Jakobsen <lmj@corti.ai>
2019-10-30 11:28:16 +0100

    adding threading to elasticsearch sending

--- example/main.go
+++ example/main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"fmt"
 	"github.com/corticph/go-logging/pkg/logging"
 	"github.com/elastic/go-elasticsearch/v8"
 	"github.com/spf13/pflag"
 	"github.com/spf13/viper"
+	"time"
 )
 
 func main() {
@@ -25,13 +27,20 @@ func main() {
 
 	setUpElasticClient()
 
-	logging.Info("Hello world!")
+	now := time.Now()
+	logging.Info("Hello info!")
+	logging.Warn("Hello warn")
+	logging.Err("Hello error")
+	logging.LogAs(logging.FATAL, "Hello fatal!")
+	logging.Info("More logging!")
 
+	fmt.Println("Sent all log messages to elasticsearch:", time.Since(now))
 }
 
 // This function *must* run before any calls to logging package
 func setUpElasticClient() {
 	if err := logging.SetElasticClient(
+		0,
 		viper.GetString("elk-service"),
 		elasticsearch.Config{
 			Addresses: []string{viper.GetString("elk-cloud-addr")},
--- go.mod
+++ go.mod
@@ -10,6 +10,8 @@ require (
 	github.com/getsentry/raven-go v0.2.0
 	github.com/google/gopacket v1.1.17 // indirect
 	github.com/magiconair/properties v1.8.1 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.1 // indirect
 	github.com/pelletier/go-toml v1.6.0 // indirect
 	github.com/prometheus/client_golang v1.2.1 // indirect
 	github.com/spf13/afero v1.2.2 // indirect
