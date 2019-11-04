03218d361bf70468f1d99e7a8e7b756d4f08d7b4
Lucas Klafke Beck <lucasklafkebeck@gmail.com>
2019-11-04 13:05:07 +0100

    adding stress test

--- example/main.go
+++ example/main.go
@@ -16,6 +16,8 @@ func main() {
 	pflag.String("elk-user", "", "Username with create access on the correct Elastic Index")
 	pflag.String("elk-pass", "", "Password for provided user")
 	pflag.Int("log-level", int(logging.INFO), "set the log level to display")
+	pflag.Int("processes", 0, "how many processes to use")
+	pflag.Bool("stress-test", false, "if to perform some stress test (send a shit ton of logs to elastic search)")
 
 	pflag.Parse()
 
@@ -34,13 +36,22 @@ func main() {
 	logging.LogAs(logging.FATAL, "Hello fatal!")
 	logging.Info("More logging!")
 
+	if viper.GetBool("stress-test") {
+		for i := 0; i < 1000; i++ {
+			time.Sleep(1)
+			logging.Info("Sending stress test logs!!")
+
+		}
+
+	}
+
 	fmt.Println("Sent all log messages to elasticsearch:", time.Since(now))
 }
 
 // This function *must* run before any calls to logging package
 func setUpElasticClient() {
 	if err := logging.SetElasticClient(
-		0,
+		viper.GetInt("processes"),
 		viper.GetString("elk-service"),
 		elasticsearch.Config{
 			Addresses: []string{viper.GetString("elk-cloud-addr")},
