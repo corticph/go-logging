6cabea14d4c33aea943d443fa941df76b15c2875
Lucas Klafke Beck <lucasklafkebeck@gmail.com>
2019-11-04 14:39:24 +0100

    adding a no blocking test

---
+++ pkg/logging/logging_test.go
@@ -0,0 +1,73 @@
+package logging
+
+import (
+	"encoding/json"
+	"github.com/elastic/go-elasticsearch/v8"
+	"io/ioutil"
+	"os"
+	"testing"
+)
+
+func readESConfigs(path string) map[string]string {
+
+	var config map[string]string
+
+	jsonFile, err := os.Open(path)
+	// if we os.Open returns an error then handle it
+	if err != nil {
+		panic(err)
+	}
+	defer jsonFile.Close()
+
+	byteValue, err := ioutil.ReadAll(jsonFile)
+
+	if err != nil {
+		panic(err)
+	}
+
+	err = json.Unmarshal(byteValue, &config)
+	if err != nil {
+		panic(err)
+	}
+
+	return config
+}
+
+// make sure we can call a bunch of logs
+// without the program blocking execution
+func TestNonBlocking(t *testing.T) {
+
+	config := readESConfigs("esconfig.json")
+
+	err := SetElasticClient(
+		0,
+		"test",
+		elasticsearch.Config{
+			Addresses: []string{config["elk-cloud-addr"]},
+			Username:  config["elk-user"],
+			Password:  config["elk-pass"],
+		})
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	for i := 0; i < 1000; i++ {
+		Info("Sending stress test logs!!")
+
+	}
+}
+
+//func TestMissingConfigs(t *testing.T) {
+//
+//	err := SetElasticClient(
+//		0,
+//		"bla",
+//		elasticsearch.Config{
+//			Addresses: []string{"test"},
+//		},
+//	)
+//
+//	Info("Hello info!")
+//
+//}
